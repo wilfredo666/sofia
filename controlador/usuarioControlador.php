6990fb0bf40e96ec5d697a65aa83755174e5e35b
Wilfredo Saez <wilfredosaez@hotmail.com>
2022-03-23 17:22:38 -0400

    23-03-22

--- controlador/usuarioControlador.php
+++ controlador/usuarioControlador.php
@@ -10,33 +10,36 @@ class ControladorUsuario{
     if(isset($_POST["ingUsuario"])){
 
       if(preg_match('/^[a-zA-Z0-9]+$/',$_POST["ingUsuario"])){
-        
+
         //recuperando los datos del formulario login
         $ipServer=$_POST["ipServidor"];
         $user=$_POST["ingUsuario"];
         $pass=$_POST["ingPassword"];
         $bd=$_POST["bdServidor"];
-
-        //verificando la conexion a nivel de base de datos
-
+        
         $host="firebird:dbname=".$ipServer.":".$bd;
-        if(!($link=new PDO($host, $user, $pass))){
-          '<script>
-                    window.location="salir";
-                </script>';
-        }else{
-          $_SESSION["iniciarSesion"]="ok";
+        
+        //verificando la conexion a nivel de base de datos
+        try{
+          $link=new PDO($host, $user, $pass);
           
+          $_SESSION["iniciarSesion"]="ok";
+
           $_SESSION["ipServer"]=$ipServer;
           $_SESSION["ingUsuario"]=$user;
           $_SESSION["ingPassword"]=$pass;
           $_SESSION["bdServidor"]=$bd;
-          
+
           echo '<script>
                     window.location="inicio";
                 </script>';
+          
+        }catch(Exception $ex){
+          echo '
+          <br><div class="alert alert-danger">Error al ingresar, vuelve a intentarlo</div>
+          ';
+          exit();
         }
-
 
       }else{
         echo '
--- ejConexion.php
+++ ejConexion.php
@@ -1,23 +1,27 @@
 <?php
 $base_url= "http://localhost/sofia/";
 
-$host="firebird:dbname=localhost:demo";
+$host="firebird:dbname=localhost:demos";
 
-$db="demo";
 $user="SOFIASYS";
 $pass="S0f1a+";
 
-$conn=new PDO($host,$user,$pass);
+//$con=new PDO($host,$user,$pass);
 /*$con=ibase_connect($host,$user,$pass);*/
-if(!$conn){
+try{
+  $con=new PDO($host,$user,$pass);
+}catch(Exception $ex){
+  echo "error: ".$ex->GetMessage();
+}
+/*if(!$conn){
   echo "Acceso incorrecto";
   exit;
-}
+}*/
 
-$sql="select * from A6";
+/*$sql="select * from A6";
 $query=$conn->query($sql);
 $us=$query->fetch();
-var_dump($us);
+var_dump($us);*/
 /*foreach($query as $row){
  echo $row["COD"]." ".$row["NIT"]." - ".$row["RAZON"];
   echo "<br>";
