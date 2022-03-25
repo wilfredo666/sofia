794e4988323b9fa0bd21a428d079b54a7f47a822
LuisFFT <luisfuentest2014@hotmail.com>
2022-03-25 12:45:28 -0400

    Formulario nuevo avanzado 25032022
    Aqui esta otro nuevo concepto de formulario

--- vista/modulos/FormFactura.php
+++ vista/modulos/FormFactura.php
@@ -26,17 +26,24 @@
       <div class="card-body">
         <form action="" class="row">
           <div class="form-group row col-md-8">
-            <div class="form-group col-md-6">
-              <label for="">Numero de Autorizacion</label> 
-              <select type="text" class="form-control">
-                <option value="">Seleccionar</option>
-              </select>
+            <div class="form-group col-md-2">
+              <label for="">Fecha</label> 
+              <input type="text" class="form-control" value="<?php echo date("d-m-Y");?>" disabled>
+            
             </div>
-            <div class="form-group col-md-6">
-              <label for="">Señor(es)</label>
-              <input type="text" class="form-control" placeholder="Codigo o nombre del Cliente" onkeyup="busCliente">
+            <div class="form-group col-md-3">
+              <label for="">Sucursal</label> 
+              <input type="text" class="form-control" placeholder="Emp_Suc.num" onkeyup="busCliente">
             </div>
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-3">
+              <label for="">Punto de Venta</label>
+              <input type="text" class="form-control" onkeyup="busCliente" placeholder="Esta en Pendiente">
+            </div>
+            <div class="form-group col-md-3">
+              <label for="">Tipo de Factura</label>
+              <input type="text" class="form-control" placeholder="Sale de la API" onkeyup="busCliente">
+            </div>
+            <!--<div class="form-group col-md-2">
               <label for="">Tipo Documento</label>
               <select type="text" class="form-control">
                 <option value="tipDocEjemplo1">Ninguno</option>
@@ -46,22 +53,55 @@
                 <option value="tipDocEjemplo5">4|OD-OTRO DOCUMENTO DE IDENTIDAD</option>
                 <option value="tipDocEjemplo6">5|NIT-NUMERO DE IDENTIFICACION TRIBUTARIA</option>
               </select>
+            </div>-->
+            <div class="form-group col-md-7">
+              <label for="">Actividad</label> 
+              <input type="text" class="form-control" placeholder="Sale de la API">
             </div>
-            <div class="form-group col-md-6">
-              <label for="">&nbsp;</label> 
-              <input type="text" class="form-control" placeholder="Escribir los datos del documento seleccionado">
+            <div class="form-group col-md-4">
+              <label for="">Email</label> 
+              <input type="text" class="form-control" placeholder="LCLI.email">
             </div>
+            <div class="form-group col-md-4">
+              <label for="">NIT</label> 
+              <div class="input-group" >
+              <input type="text" class="form-control" placeholder="LCLI.NIT">
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">
+                  <i class="fas fa-search">  
+              </i>
+              </button>
+              </div>
+             <!-- <button type="button" class="btn btn-outline-primary btn-block"> 
+              <i class="fas fa-search">  
+              </i>
+              </button>-->
+              </div>
+            </div>
+
             <div class="form-group col-md-6">
+              <label for="">Nombre o Razon social</label> 
+              <div class="input-group" >
+              <input type="text" class="form-control" placeholder="LCLI.razon'validar con API' devuelve por API">
+               <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">
+                  <i class="fas fa-search">  
+                  </i>
+                </button>
+               </div>
+              </div>
+            </div>
+            <!--<div class="form-group col-md-6">
               <label for="">Observaciones</label> 
               <input type="text" class="form-control" placeholder="Observaciones respecto a la emision (opcional)">
-            </div>
-            <div class="form-group col-md-6">
+            </div>-->
+            <!--<div class="form-group col-md-6">
               <label for="">Tipo de pago</label> 
               <select type="text" class="form-control">
                 <option value="">Efectivo</option>
                 <option value="" selected>Credito</option>
               </select>
-            </div>
+            </div>-->
           </div>
           <!--datos de la empresa emisora-->
           <div class="form-group col-md-4">
@@ -86,21 +126,45 @@
     <div class="card">
       <div class="card-body">
         <form action="" class="row" id="formDetalle">
-          <div class="form-group col-md-5">
+          <div class="form-group col-md-2">
+            <label for="">Codigo</label>
+            <div class="input-group" >
+              <input type="text" class="form-control" placeholder="IPROD.COD">
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">
+                  <i class="fas fa-search">  
+                  </i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="form-group col-md-2">
             <label for="">Concepto</label>
-            <input type="text" class="form-control" id="DesProducto" name="DesProducto" placeholder="Buscar por codigo o descripción">
+            <input type="number" class="form-control" id="ConcProducto" name="ConcProducto" placeholder="IPROD.Nombre">
           </div>
-          <div class="form-group col-md-2">
+          <div class="form-group col-md-1">
             <label for="">Cantidad</label>
-            <input type="number" class="form-control" id="CantProducto" name="CantProducto">
+            <input type="number" class="form-control" id="CantProducto" name="CantProducto" placeholder="Entra a FFACTURA.Cantidad">
           </div>
           <div class="form-group col-md-2">
+            <label for="">Unidad de Medida</label>
+            <input type="number" class="form-control" id="UniMedProducto" name="UniMedProducto" placeholder="Entra a FFACTURA.Unidad">
+          </div>
+          <div class="form-group col-md-1">
             <label for="">P. Unitario</label>
-            <input type="text" class="form-control" id="PreUnitario" name="PreUnitario">
+            <input type="text" class="form-control" id="PreUnitario" name="PreUnitario" placeholder="Entra a FFACTURA.Precio">
           </div>
-          <div class="form-group col-md-2">
+          <div class="form-group col-md-1">
+            <label for="">Descuento</label>
+            <input type="text" class="form-control" id="Descuento" name="Descuento" placeholder="FFACTURA.descuento">
+          </div>
+          <div class="form-group col-md-1">
+            <label for="">Lote</label>
+            <input type="text" class="form-control" id="LoteProd" name="LoteProd" placeholder="FFACTURA.Lote">     
+          </div>
+          <div class="form-group col-md-1">
             <label for="">P. Total</label>
-            <input type="text" class="form-control" id="PreTotal" name="PreTotal" readonly>     
+            <input type="text" class="form-control" id="PreTotal" name="PreTotal" readonly placeholder="FFACTURA.Total">     
           </div>
           <div class="form-group col-md-1">
             <label for="">&nbsp;</label>
