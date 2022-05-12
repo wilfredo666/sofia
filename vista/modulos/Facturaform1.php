27c10005b01483477efb5800a4fb6907dc194ea0
LuisFFT <luisfuentest2014@hotmail.com>
2022-05-12 07:36:03 -0400

    falta foreach y fetchall

--- vista/modulos/Facturaform1.php
+++ vista/modulos/Facturaform1.php
@@ -10,6 +10,17 @@ FROM EMPRESA");
 $stmt->execute();
 $empresa=$stmt->fetch();
 
+$sql_datos_adicionales=Conexion::conectar()->query("SELECT nitcli, nomfact, nfac, control
+FROM FCTROLF");
+$sql_datos_adicionales->execute();
+$Datos_adicionales=$sql_datos_adicionales->fetch();
+
+$datostabla=Conexion::conectar()->query("SELECT cod, cantidad, concepto, precio, descto, numeroserie, actecon, total
+FROM FFACTURA where CUF='2'");
+$datostabla->execute();
+$Datos_en_tabla=$datostabla->fetch();
+
+
 $sql_factura=Conexion::conectar()->query("SELECT nombre, nit_ruc, direccion
 FROM EMPRESA");
 $sql_factura->execute();
@@ -52,7 +63,7 @@ $pdf->setY(50);$pdf->setX(10);
 /*$date = date("D M d, Y G:i");*/
 $pdf->Cell(5,$textypos,utf8_decode("Fecha: ".$date));//Telefono de la empresa
 $pdf->setY(55);$pdf->setX(10);
-$pdf->Cell(5,$textypos,utf8_decode("Nombre/Razon Social: "));//Email de la empresa
+$pdf->Cell(5,$textypos,utf8_decode("Nombre/Razon Social: ".$Datos_adicionales ['NOMFACT']));//Email de la empresa
 
 // Agregamos los datos del cliente
 $pdf->SetFont('Arial','B',10);    
@@ -73,14 +84,14 @@ $pdf->SetFont('Arial','B',10);
 $pdf->setY(5);$pdf->setX(135);
 $pdf->Cell(5,$textypos,utf8_decode("NIT:                             ".$empresa["NIT_RUC"]));
 $pdf->setY(10);$pdf->setX(135);
-$pdf->Cell(5,$textypos,"FACTURA                       #12345");
+$pdf->Cell(5,$textypos,utf8_decode("Factura:                         ".$Datos_adicionales ['NFAC']));
 $pdf->setY(15);$pdf->setX(135);
-$pdf->Cell(5,$textypos,"Cod. Autorizacion          #12345");
+/*$pdf->Cell(5,$textypos,"Cod. Autorizacion          #12345");*/
 $pdf->SetFont('Arial','',10);    
 $pdf->setY(45);$pdf->setX(135);
 /*$pdf->Cell(5,$textypos,"NIT: 7554422867");*/
 $pdf->setY(50);$pdf->setX(135);
-$pdf->Cell(5,$textypos,"NIT: 7554422867");
+$pdf->Cell(5,$textypos,utf8_decode("NIT: ".$Datos_adicionales["NITCLI"]));
 $pdf->setY(55);$pdf->setX(135);
 $pdf->Cell(5,$textypos,"");
 $pdf->setY(60);$pdf->setX(135);
@@ -94,17 +105,21 @@ $pdf->setY(60);$pdf->setX(135);
 $header = array("Cod. Producto", "Cantidad","Descripcion","Precio Uni","Descuento","ICE%","ICE ESP","Subtotal");
 //// Arrar de Productos
 $products = array(
-	array("0010", "Producto 1",3,120,0,0),
-	array("0024", "Producto 2",5,80,0,0),
-	array("0001", "Producto 3",1,40,0,0),
-	array("0001", "Producto 3",5,80,0,0),
-	array("0001", "Producto 3",4,30,0,0),
-	array("0001", "Producto 3",7,80,0,0),
+    
+    /*while($Datos_en_tabla as $value){*/
+    array($Datos_en_tabla["COD"], number_format($Datos_en_tabla["CANTIDAD"]), $Datos_en_tabla['CONCEPTO'], number_format($Datos_en_tabla['PRECIO']) , $Datos_en_tabla['DESCTO'], $Datos_en_tabla['NUMEROSERIE']),
+    /*}*/
+	array("0010", 1,3,120,0,0),
+	array("0024", 2,5,80,0,0),
+	array("0001", 3,1,40,0,0),
+	array("0001", 4,5,80,0,0),
+	array("0001", 11,4,30,0,0),
+	array("0001", 5,7,80,0,0),
 );
 /*$this->pdf->Image(FCPATH."Sofia.jpg",20,50,150,170);//logo para membretado*/
 				/*$y=$y+25;*/
     // Column widths
-    $w = array(25, 30, 20, 20, 25, 25,20,25);
+    $w = array(25, 20, 30, 20, 25, 25,20,25);
     // Header
     for($i=0;$i<count($header);$i++)
         $pdf->Cell($w[$i],7,$header[$i],1,0,'C');
@@ -115,14 +130,20 @@ $products = array(
     {
         $pdf->Cell($w[0],6,$row[0],1);
         $pdf->Cell($w[1],6,$row[1],1);
-        $pdf->Cell($w[2],6,number_format($row[2]),'1',0,'R');
+        $pdf->Cell($w[2],6,$row[2],1);
+        $pdf->Cell($w[3],6,$row[3],1);
+        $pdf->Cell($w[4],6,$row[4],1);
+        $pdf->Cell($w[5],6,$row[5],1);
+        $pdf->Cell($w[6],6,"$ ".number_format($row[1]*$row[1],2,".",","),'1',0,'R');
+        $pdf->Cell($w[7],6,"$ ".number_format($row[1]*$row[3],2,".",","),'1',0,'R');
+       /* $pdf->Cell($w[2],6,number_format($row[2]),'1',0,'R');
         $pdf->Cell($w[3],6,"$ ".number_format($row[3],2,".",","),'1',0,'R');
         $pdf->Cell($w[4],6,"$ ".number_format($row[3]*$row[2],2,".",","),'1',0,'R');
         $pdf->Cell($w[5],6,"$ ".number_format($row[3]*$row[2],2,".",","),'1',0,'R');
         $pdf->Cell($w[6],6,"$ ".number_format($row[3]*$row[2],2,".",","),'1',0,'R');
-        $pdf->Cell($w[7],6,"$ ".number_format($row[3]*$row[2],2,".",","),'1',0,'R');
+        $pdf->Cell($w[7],6,"$ ".number_format($row[3]*$row[2],2,".",","),'1',0,'R');*/
         $pdf->Ln();
-        $total+=$row[3]*$row[2];
+        /*$total+=$row[3]*$row[2];*/
 
     }
 /////////////////////////////
